59f73359804e1c3d29564f483f8c0ae70a601f76
Yonas Adiel Wiguna <yonazadielwiguna@gmail.com>
2018-04-12 06:40:52 +0700

    resolve conlict

--- test.cpp
+++ test.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include "tests/Coin.test.hpp"
 #include "tests/Data.test.hpp"
+#include "tests/Food.test.hpp"
+#include "tests/Snail.test.hpp"
 #include "tests/Position.test.hpp"
 #include "tests/LinkedList.test.hpp"
-<<<<<<< HEAD
-#include "tests/Snail.test.hpp"
-=======
-#include "tests/Food.test.hpp"
 #include "tests/LinkedListItem.test.hpp"
->>>>>>> 0323b6d4cedd1ce70207a01164f67f743bb1e6a7
 
 int main() {
     int countFail = 0;
